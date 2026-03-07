d767f484b7c5d7fc4332beb4db2f7f6c14cdb8ea
Akshay Daydar <139338267+adaydar@users.noreply.github.com>
2026-03-07 17:57:48 +0530

    Update train_knee.sh

--- RRG/knee/train_knee.sh
+++ RRG/knee/train_knee.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #export CUDA_VISIBLE_DEVICES=2
-python3 main.py --epochs 50 --lr_backbone 1e-5 --lr 1e-4 --batch_size 2 --image_size 300 --vocab_size 760 --theta 0.4 --gamma 0.4 --beta 1.0 --delta 0.01 --dataset_name knee_xray --t_model_weight_path ./weight_path/iutmodel_new.pth --anno_path ../kneeXray/annotation_255_tab1.json --data_dir ../kneeXray/OAI_dataset_255/images --mode train
+python3 main.py --epochs 50 --lr_backbone 1e-5 --lr 1e-4 --batch_size 2 --image_size 300 --vocab_size 760 --theta 0.4 --gamma 0.4 --beta 1.0 --delta 0.01 --dataset_name knee_xray --t_model_weight_path ./weight_path/iutmodel_new.pth --anno_path ../classification/knee/predictor_module/dataset/annotation_255_tab1.json --data_dir ..//classification/knee/predictor_module/dataset/images --mode train
 
